b8a5aebeb0412e42a267ce70ab397b1311e2a742
Asdrubal <asdrubal.a.l@gmail.com>
2016-04-05 13:38:52 -0430

    actualizacion 05/04/2016

--- js/main.js
+++ js/main.js
@@ -1,71 +1,6 @@
 /**
- * Main AngularJS Web Application
- */
-var app = angular.module('matApp', ['ngRoute', 'ngDialog']);
-
-/**
- * Configure the Routes
- */
-app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    // Home
-    .when("/", {templateUrl: "templates/sidebar.html", controller: "PageCtrl"})
-    // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
-    // else 404
-    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
-}]);
-
-/**
- * Controls the Blog
- */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
-
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
-});
-
-/**
- * Modal box
- */
-
-app.controller('MainCtrl', function ($scope, ngDialog) {
-  $scope.openContactForm = function() {
-    ngDialog.openConfirm({template: '../partials/about.html',
-      scope: $scope //Pass the scope object if you need to access in the template
-    }).then(
-      function(value) {
-        //save the contact form
-      },
-      function(value) {
-        //Cancel or do nothing
-      }
-    );
-  };
-});
-
+Toogle Wrapper
+*/
 $(document).ready(function(){
   $("span#menu-toggle").on('click',function(e) {
     e.preventDefault();
@@ -73,24 +8,158 @@ $(document).ready(function(){
   });
 });
 
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('matApp', ['ngRoute', 'ngDialog', 'angular-carousel-3d']);
+
+/**
+ * Modal box sidebar
+ */
 app.directive('sidebar', function(ngDialog) {
   return {
     restrict: 'E',
     templateUrl: './templates/sidebar.html',
     replace: true,
     link: function ($scope) {
-      $scope.openContactForm = function() {
-        ngDialog.openConfirm({template: './partials/about.html',
-        scope: $scope //Pass the scope object if you need to access in the template
-      }).then(
-        function(value) {
-        //save the contact form
-        },
-        function(value) {
-        //Cancel or do nothing
-        }
-      );
+      $scope.openDialog = function(plantilla) {
+        ngDialog.openConfirm({
+          template: './partials/'+plantilla+'.html',
+          scope: $scope,
+      });
       }
     }
   }
 });
+
+app.directive('content', function() {
+  return {
+    restrict: 'E',
+    templateUrl: './templates/content.html',
+
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Configure the Routes
+ */
+//app.config(['$routeProvider', function ($routeProvider) {
+  //$routeProvider
+    // Home
+    //.when("/", {templateUrl: "templates/sidebar.html", controller: "PageCtrl"})
+    // Pages
+    //.when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    //.when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    //.when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
+    //.when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    //.when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // Blog
+   // .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    //.when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    // else 404
+    //.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+//}]);
+
+/**
+ * Controls the Blog
+ */
+//app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+//  console.log("Blog Controller reporting for duty.");
+//});
+
+/**
+ * Controls all other Pages
+ */
+//app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+ // console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  //$('.carousel').carousel({
+   // interval: 5000
+  //});
+
+  // Activates Tooltips for Social Links
+ // $('.tooltip-social').tooltip({
+  //  selector: "a[data-toggle=tooltip]"
+ // })
+//});
